f5af066ac0e4c2b6a36c2e3813cafe002cd40f41
Rexdey <rexdey@gmail.com>
2025-09-19 14:50:35 -0300

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "db_name" {
+  default = "usersdb"
+}
+
+variable "db_user" {
+  default = "postgres"
+}
+
+variable "db_password" {
+  default = "password123"
+}
